f6f79a0544d0c2fa4d2979282835025c96766028
Muhammad Alyan <alyan0332@gmail.com>
2023-07-19 12:35:34 +0500

    add app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import LandingPage from './components/landingPage';
 import Navbar from './components/navbar';
+import AboutUs from './components/aboutUs';
+import Careers from './components/careers';
+import Services from './components/services';
+import OurWork from './components/ourWork';
+import Footer from './components/footer';
+import { ContactUs } from './components/contactUs';
+import Blogs from './components/blogs';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <LandingPage />
-      <div style={{ height: '200vh' }}></div>
+      <AboutUs />
+      <Careers />
+      <Services />
+      <OurWork />
+      <Blogs />
+      <ContactUs />
+      <Footer />
     </div>
   );
 }
