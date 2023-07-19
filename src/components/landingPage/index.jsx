7e4fb77748caaf3cd2a97e6086fe25858e566a31
Muhammad Alyan <alyan0332@gmail.com>
2023-07-19 12:31:52 +0500

    add landing page

--- src/components/landingPage/index.jsx
+++ src/components/landingPage/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import backgroundVideo from '../../assets/paint-explosion.mp4';
+import backgroundVideo from '../../assets/video7.mp4';
 import './landingPage.css';
+import { InView } from 'react-intersection-observer';
+import { useState } from 'react';
 
 const LandingPage = () => {
+  const [inView, setInView] = useState(false);
   return (
-    <div className="hero-section">
+    <div className="hero-section" id="home">
       <div className="hero-section__overlay"></div>
       <video
         className="hero-section__video"
@@ -14,8 +17,9 @@ const LandingPage = () => {
         muted
       ></video>
       <section className="hero-section__content">
-        <h1 className="hero-section__title">
-          Colossal Innovation - Unleashing Business Potential through Innovation
+        <h2 className="hero-section__company-name">Colossal Innovation</h2>
+        <h1 className="hero-section__slogan">
+          Unleashing Business Potential through Innovation
         </h1>
         <p className="hero-section__description">
           Transform Your Business with Colossal Innovation. We are a leading
@@ -23,7 +27,21 @@ const LandingPage = () => {
           organizations to achieve unparalleled success. Harness our expertise
           and cutting-edge solutions to unlock your business's full potential.
         </p>
-        <button className="button">Get in Touch</button>
+        <InView
+          onChange={setInView}
+          threshold={1}
+          className={inView ? 'slide-up-fade--slow' : 'hidden-bottom'}
+        >
+          <a
+            role="button"
+            href="#contactUs"
+            className={`button ${
+              inView ? 'slide-up-fade--slow' : 'hidden-bottom'
+            }`}
+          >
+            Get in Touch
+          </a>
+        </InView>
       </section>
     </div>
   );
